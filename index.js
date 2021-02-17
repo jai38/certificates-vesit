757030370a09fba207de511f3fae428d10458e3a
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-02-18 00:01:44 +0530

    removed nm

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const app = express();
+const router = express.Router();
+// const helpers = require("helpers");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("./../index");
+});
+
+app.post("/", (req, res) => {
+  let upload = multer({
+    storage: storage,
+    // fileFilter: helpers.imageFilter,
+  }).single("allFiles");
+  upload(req, res, (err) => {
+    res.send("Done");
+  });
+});
+console.log();
+app.listen(5000, console.log("On port 5000"));
