fd401bb218424bf2ce3cb8356b7488dc263b7183
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-02-22 14:46:23 +0530

    csv to json

--- routes/uploadCsv.js
+++ routes/uploadCsv.js
@@ -1,17 +1,40 @@
 const router = require("express").Router();
 const storage = require("../storage");
 const multer = require("multer");
-
+const fs = require("fs");
+let users = [];
+let fileName;
+var upload = multer({
+  storage: storage,
+});
+const uploadUID = async (fileName) => {
+  const csv = fs.readFileSync(`../uploads/${fileName}`, "utf-8");
+  papaparse.parse(csv, {
+    complete: (sheet) => {
+      sheets = sheet.data.shift();
+      sheets = sheet.data;
+    },
+  });
+  sheets.forEach(async (c) => {
+    let currentUser = {
+      UID: c[0],
+      name: c[3],
+      year: 2020,
+    };
+    users.push(currentUser);
+  });
+  return users;
+};
 router.get("/", (req, res) => {
   res.render("dashboard");
 });
-router.post("/", (req, res) => {
-  let uploadCsv = multer({
-    storage: storage,
-  }).single("csv");
-  uploadCsv(req, res, (err) => {
-    res.send("done");
-  });
+router.post("/", upload.single("csv"), (req, res, next) => {
+  const file = req.file;
+  console.log(file);
+  fileName = file.filename;
+  users = uploadUID();
+  console.log(users);
+  res.send("done");
 });
 
 module.exports = router;
