c81038eac205c7b5958c3cd1ba0974f056007341
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-04-10 18:05:02 +0530

    final table

--- public/clientJS/userData.js
+++ public/clientJS/userData.js
@@ -30,7 +30,7 @@ const getData = () => {
     <td>${c.year}</td>
     <td>${c.name}</td>
     <td><a class='btn btn-success' href='${c.link}' target="_blank">Certificate</td>
-    <td><button class='btn btn-danger' data-toggle="modal" data-target="#myModal" onclick='deleteUser("${c.email}","${c.UID}","${c.fileName}")'>Delete</button></td>
+    <td><button class='btn btn-danger m-0' data-toggle="modal" data-target="#myModal" onclick='deleteUser("${c.email}","${c.UID}","${c.fileName}")'>Delete</button></td>
       </tr>`;
   });
   tbody.innerHTML = html;
